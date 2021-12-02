9311c93ae17adc7b2760b03530a26fa0bb06a726
Jonathan Toledo Orobia <dev.toledorobia@outlook.com>
2021-12-01 22:25:59 -0300

    second try app service connect mongodb atlas

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 config();
+
+console.log("MONGODB_URI", process.env.MONGODB_URI);
 
 export default {
   mongodbURI: process.env.MONGODB_URI,
   tokenSecret: process.env.TOKEN_SECRET,
-}
+};
