558a6fe7f748e6b532486ce5565ba1f13980e4df
Jonathan Toledo Orobia <dev.toledorobia@outlook.com>
2021-12-15 02:06:25 -0300

    test heroku 4

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,6 @@
 import User from "./user";
 import Post from "./post";
 import Comment from "./comment";
-import Token from "./Token";
+import Token from "./token";
 
 export { User, Post, Comment, Token };
